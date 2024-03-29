99ae5106fa40dfc6d33f99f35dfb34457d61df79
tzh00203 <tzh00203@163.com>
2024-03-29 12:38:57 +0800

    add 6

---
+++ _6_annotating/_6_annotating_iot.py
@@ -0,0 +1,25 @@
+
+
+def label_type():
+    # TODO
+    ...
+
+
+def label_vendor():
+    # TODO
+    ...
+
+
+def label_product(words_list):
+    for word in words_list:
+        ...
+
+
+
+
+
+
+
+
+
+
